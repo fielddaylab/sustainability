71e7f4c9aef6440080b226313204e5d6fd3668fd
Ricky Mutschlechner <mutschlechne@wisc.edu>
2013-09-16 17:28:08 -0500

    Working on game logic.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,58 @@
+console.log("Beginning");
 var budgetDiv = document.getElementById("budget");
-budgetDiv.innerHTML = "Budget: " + budget;
-
 var timeRemainingDiv = document.getElementById("timeRemaining");
-timeRemainingDiv.innerHTML = "Time Remaining: " + timeRemaining + " Months";
-
 var satisfactionDiv = document.getElementById("satisfaction");
-satisfactionDiv.innerHTML = "Satisfaction: " + satisfaction + "%";
-
 var waterWasteRateDiv = document.getElementById("waterWasteRate");
-waterWasteRateDiv.innerHTML = "Water Waste Rate: " + waterWasteRate + " gallons per month";
-
 var waterPlantCapacityDiv = document.getElementById("waterPlantCapacity");
-waterPlantCapacityDiv.innerHTML = "Water Plant Initial Capacity: " + waterPlantCapacity + " gallons";
+var waterPlantNetCapacityDiv = document.getElementById("waterPlantNetCapacity"); 
+var irrigateAtNightDiv = document.getElementById("irrigateNight");
+var waterNetCapacity = waterPlantCapacity - waterWasteRate; //the current capacity, after waste
 
-var waterPlantNetCapacityDiv = document.getElementById("waterPlantNetCapacity"); //the current capacity, after waste
+updateAll(); //Initialize all of our divs with their respective values
 
-var waterNetCapacity = waterPlantCapacity - waterWasteRate;
-waterPlantNetCapacityDiv.innerHTML = "Water Plant Net Capacity: " + waterNetCapacity + " gallons";
+function updateAll(){
+	
+	
+	waterNetCapacity = waterPlantCapacity - waterWasteRate;
+	
+	budgetDiv.innerHTML = "Budget: " + budget;
+	
+	timeRemainingDiv.innerHTML = "Time Remaining: " + timeRemaining + " Months";
+	
+	satisfactionDiv.innerHTML = "Satisfaction: " + satisfaction + "%";
+	
+	waterWasteRateDiv.innerHTML = "Water Waste Rate: " + waterWasteRate + " gallons per month";
+	
+	waterPlantCapacityDiv.innerHTML = "Water Plant Initial Capacity: " + waterPlantCapacity + " gallons";
+	waterPlantNetCapacityDiv.innerHTML = "Water Plant Net Capacity: " + waterNetCapacity + " gallons";
+	irrigateAtNightDiv.innerHTML = "Irrigate at night: " + (25 * irrigateAtNight.count) + "% of campus 		currently";
+	
+}
+
+
+
+function addNightIrrigation(){
+	if(readyToAdd() && irrigateAtNight.count < 4){
+		//Check to make sure that we aren't already at 100%
+		irrigateAtNight.count++;
+		irrigateAtNightDiv.innerHTML = "Irrigate at night: " + (25 * irrigateAtNight.count) + 									"% of campus currently";
+		budget += (irrigateAtNight.cost + irrigateAtNight.costMaintain); 
+		waterWasteRate -= irrigateAtNight.waterSaved;
+		satisfaction += irrigateAtNight.satisfaction;
+		updateAll();
+	}
+}
+
+
+function readyToAdd(){
+	//Check to make sure that we're able to currently add things
+	if(budget > 0){
+		if(satisfaction > 0){
+			if(waterNetCapacity > 0)
+				return true;
+		}
+	}
+	
+	return false;
+	
+}
--- js/variables.js
+++ js/variables.js
@@ -3,3 +3,57 @@ var timeRemaining = 12; //This is the starting time remaining in Months, default
 var satisfaction = 50; //This is the starting satisfaction (in %), default 100
 var waterWasteRate = 800; //The starting water waste rate in gal/mo, default 0
 var waterPlantCapacity = 1000; // The capacity of the water filtration plant in gallons, default 1000.
+
+// Irrigate at night
+var irrigateAtNight = { 
+	cost: 0,
+	costMaintain: 0,
+	waterSaved: 200,
+	satisfaction: 0,
+	count: 0
+};
+
+// Irrigate less
+var irrigateLess = { 
+	cost: -100000,
+	costMaintain: 0,
+	waterSaved: 400,
+	satisfaction: -25,
+	count: 0
+};
+
+// Dual flush toilet
+var dualFlushToilet = { 
+	cost: 100000,
+	costMaintain: 0,
+	waterSaved: 200,
+	satisfaction: 0,
+	count: 0
+};
+
+// Faucet sensors
+var faucetSensors = { 
+	cost: 100000,
+	costMaintain: 15000,
+	waterSaved: 100,
+	satisfaction: 0,
+	count: 0
+};
+
+// Green roof
+var greenRoof = { 
+	cost: 200000,
+	costMaintain: 25000,
+	waterSaved: 0,
+	satisfaction: 50,
+	count: 0
+};
+
+// 
+var cistern = { 
+	cost: 100000,
+	costMaintain: 25000,
+	waterSaved: 200,
+	satisfaction: 0,
+	count: 0
+};
