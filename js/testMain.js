8a3ad97cdea42a7754e01cc2346bd290aa286c01
Ricky Mutschlechner <mutschlechne@wisc.edu>
2013-11-18 16:35:54 -0600

    Split up JS logic into testMain.js... leaving main.js, but probably not using it anymore once everythig is ported over.

--- js/testMain.js
+++ js/testMain.js
@@ -1,51 +1,154 @@
-console.log("Test environment.");
+var refreshBuildings;
 
-var divs = {};
+$(document).ready(function() { 
+	var header1 = $("#header1")[0]; //Building1 div
+	var header2 = $("#header2")[0]; //Building2 div
+	var header3 = $("#header3")[0]; //Building3 div
+	var header4 = $("#header4")[0]; //Building4 div
+	var header5 = $("#header5")[0]; //Building5 div
 
-divs.building1 = document.getElementById("building1");
-divs.building2 = document.getElementById("building2");
-divs.building3 = document.getElementById("building3");
-divs.building4 = document.getElementById("building4");
-divs.building5 = document.getElementById("building5");
+	var cells = $("td"); //Save the entire table
 
-divs.budget = document.getElementById("budget");
-divs.timeSurvived = document.getElementById("timeSurvived");
-divs.satisfaction = document.getElementById("satisfaction");
-divs.waterPlantCapacity = document.getElementById("waterPlantCapacity");
-divs.waterWasteRate = document.getElementById("waterWasteRate");
-divs.waterPlantNetCapacity = document.getElementById("waterPlantNetCapacity");
-divs.rainAmount = document.getElementById("rainAmount");
+	var upgradeIndex = 0;
+
+	//Giving building1 a few upgrades
+	console.log("Debug: Giving Building 1 two upgrades: Porous Pavement and Cisterns");
+	buildings.addUpgrade(buildings.building1, upgrades.porousPavement);
+	buildings.addUpgrade(buildings.building1, upgrades.cistern);
+
+	//Unlocking building1
+	console.log("Debug: Unlocking Building 1.");
+	buildings.unlock(buildings.building1);
+
+	refreshBuildings = function(){
+	//Loop through all the upgrades:
+	upgradeIndex = 0;
+	console.log("Cells.length: " + cells.length);
+	console.log(cells[0]);
+	console.log(cells[1]);
 
 
-buildings.addUpgrade(buildings.building1, upgrades.irrigateAtNight);
-buildings.addUpgrade(buildings.building1, upgrades.irrigateLess);
+	for(var i = 0; i < cells.length; i++){
+		//If we're working with a Building 1 cell:
+		if(cells[i].id == "building1"){
 
-refresh();
+			//If Building 1 is actually unlocked:
+			if(buildings.building1.unlocked){
+
+				//If we actually have the upgrade.. (This should be changed)
+				if(upgradeIndex+1 <= buildings.building1.upgrades.length){
+					cells[i].innerHTML = buildings.building1.upgrades[upgradeIndex].name;
+				}
+
+				//If we do not have the upgrade:
+				else
+					cells[i].innerHTML = "Upgrade not yet unlocked.";
+			}
+
+			else
+				cells[i].innerHTML = "Building not yet unlocked.";
+		}
 
 
-function refresh(){
-divs.building1.innerHTML = "<br/><b>Building 1: </b>" + buildings.building1.name + 
-							"<b><br/>Unlocked? </b>" + buildings.building1.unlocked +
-							"<b><br/> Upgrades: </b>" +  
-							buildings.getOwnedUpgrades(buildings.building1);
+		//If we're working with a Building 2 cell:
+		if(cells[i].id == "building2"){
 
-divs.building2.innerHTML = "<b><br/>Building 2: </b>" + buildings.building2.name + 
-							"<b><br/>Unlocked? </b>" + buildings.building2.unlocked +
-							"<b><br/> Upgrades: </b>" +  
-							buildings.getOwnedUpgrades(buildings.building2);
+			//If Building 2 is actually unlocked:
+			if(buildings.building2.unlocked){
 
-divs.building3.innerHTML = "<b><br/>Building 3: </b>" + buildings.building3.name + 
-							"<b><br/>Unlocked? </b>" + buildings.building3.unlocked+
-							"<b><br/> Upgrades: </b>" +  
-							buildings.getOwnedUpgrades(buildings.building3);
+				//If we actually have the upgrade.. (This should be changed)
+				if(upgradeIndex+1 <= buildings.building2.upgrades.length){
+					cells[i].innerHTML = buildings.building2.upgrades[upgradeIndex].name;
+				}
 
-divs.building4.innerHTML = "<b><br/>Building 4: </b>" + buildings.building4.name + 
-							"<b><br/>Unlocked? </b>" + buildings.building4.unlocked+
-							"<b><br/> Upgrades: </b>" +  
-							buildings.getOwnedUpgrades(buildings.building4);
+				//If we do not have the upgrade:
+				else
+					cells[i].innerHTML = "Upgrade not yet unlocked.";
+			}
 
-divs.building5.innerHTML = "<b><br/>Building 5: </b>" + buildings.building5.name + 
-							"<b><br/>Unlocked? </b>" + buildings.building5.unlocked+
-							"<b><br/> Upgrades: </b>" +  
-							buildings.getOwnedUpgrades(buildings.building5);
+			else
+				cells[i].innerHTML = "Building not yet unlocked.";
+		}
+
+		//If we're working with a Building 3 cell:
+		if(cells[i].id == "building3"){
+
+			//If Building 3 is actually unlocked:
+			if(buildings.building3.unlocked){
+
+				//If we actually have the upgrade.. (This should be changed)
+				if(upgradeIndex+1 <= buildings.building3.upgrades.length){
+					cells[i].innerHTML = buildings.building3.upgrades[upgradeIndex].name;
+				}
+
+				//If we do not have the upgrade:
+				else
+					cells[i].innerHTML = "Upgrade not yet unlocked.";
+			}
+
+			else
+				cells[i].innerHTML = "Building not yet unlocked.";
+		}
+
+		//If we're working with a Building 4 cell:
+		if(cells[i].id == "building4"){
+
+			//If Building 1 is actually unlocked:
+			if(buildings.building4.unlocked){
+
+				//If we actually have the upgrade.. (This should be changed)
+				if(upgradeIndex+1 <= buildings.building4.upgrades.length){
+					cells[i].innerHTML = buildings.building4.upgrades[upgradeIndex].name;
+				}
+
+				//If we do not have the upgrade:
+				else
+					cells[i].innerHTML = "Upgrade not yet unlocked.";
+			}
+
+			else
+				cells[i].innerHTML = "Building not yet unlocked.";
+		}
+
+		//If we're working with a Building 5 cell:
+		if(cells[i].id == "building5"){
+
+			//If Building 5 is actually unlocked:
+			if(buildings.building5.unlocked){
+
+				//If we actually have the upgrade.. (This should be changed)
+				if(upgradeIndex+1 <= buildings.building5.upgrades.length){
+					cells[i].innerHTML = buildings.building5.upgrades[upgradeIndex].name;
+				}
+
+				//If we do not have the upgrade:
+				else
+					cells[i].innerHTML = "Upgrade not yet unlocked.";
+			}
+
+			else
+				cells[i].innerHTML = "Building not yet unlocked.";
+		}
+
+		if(i >= 4 && i % 4 == 0)
+			upgradeIndex++;
+	}
+
 }
+
+
+
+	refreshBuildings(); //initially load the buildings
+	
+	//Debug output; just to make sure our first cell is getting set correctly
+	console.log(cells.get(0).id);
+
+
+	//Set all of the building names into the proper cells
+	header1.innerHTML = buildings.building1.name;
+	header2.innerHTML = buildings.building2.name;
+	header3.innerHTML = buildings.building3.name;
+	header4.innerHTML = buildings.building4.name;
+	header5.innerHTML = buildings.building5.name;
+
+});
