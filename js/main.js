01fb371d632f02b8292f048ed582114fe479bce5
Ricky Mutschlechner <mutschlechne@wisc.edu>
2013-12-29 01:18:08 -0600

    Merged the two index pages, removed all errors that I could see, now I just have to actually make the table get updated rather than the bars from earlier.

--- js/main.js
+++ js/main.js
@@ -26,8 +26,162 @@ var cisternBarDiv = document.getElementById("cisternBar");
 var waterNetCapacity = waterPlantCapacity - waterWasteRate; //the current capacity, after waste
 
 
-
+initializeTables();
 updateAll(); //Initialize all of our divs with their respective values
+
+function initializeTables(){
+    var refreshBuildings;
+    var header1 = $("#header1")[0]; //Building1 div
+    var header2 = $("#header2")[0]; //Building2 div
+    var header3 = $("#header3")[0]; //Building3 div
+    var header4 = $("#header4")[0]; //Building4 div
+    var header5 = $("#header5")[0]; //Building5 div
+
+    var cells = $("td"); //Save the entire table
+
+    var upgradeIndex = 0;
+
+    //Unlocking building1
+    console.log("Debug: Unlocking Building 1.");
+    buildings.unlock(buildings.building1);
+
+
+    //Giving building1 a few upgrades
+    console.log("Debug: Giving Building 1 two upgrades: Porous Pavement and Cisterns");
+    buildings.addUpgrade(buildings.building1, upgrades.porousPavement);
+    buildings.addUpgrade(buildings.building1, upgrades.cistern);
+
+    refreshBuildings = function () {
+        //Loop through all the upgrades:
+        upgradeIndex = 0;
+        console.log("Cells.length: " + cells.length);
+        console.log(cells[0]);
+        console.log(cells[1]);
+
+
+        for (var i = 0; i < cells.length; i++) {
+            //If we're working with a Building 1 cell:
+            if (cells[i].id == "building1") {
+
+                //If Building 1 is actually unlocked:
+                if (buildings.building1.unlocked) {
+
+                    //If we actually have the upgrade.. (This should be changed)
+                    if (upgradeIndex + 1 <= buildings.building1.upgrades.length) {
+                        cells[i].innerHTML = buildings.building1.upgrades[upgradeIndex].name;
+                    }
+
+                    //If we do not have the upgrade:
+                    else
+                        cells[i].innerHTML = "Upgrade not yet unlocked.";
+                }
+
+                else
+                    cells[i].innerHTML = "Building not yet unlocked.";
+            }
+
+
+            //If we're working with a Building 2 cell:
+            if (cells[i].id == "building2") {
+
+                //If Building 2 is actually unlocked:
+                if (buildings.building2.unlocked) {
+
+                    //If we actually have the upgrade.. (This should be changed)
+                    if (upgradeIndex + 1 <= buildings.building2.upgrades.length) {
+                        cells[i].innerHTML = buildings.building2.upgrades[upgradeIndex].name;
+                    }
+
+                    //If we do not have the upgrade:
+                    else
+                        cells[i].innerHTML = "Upgrade not yet unlocked.";
+                }
+
+                else
+                    cells[i].innerHTML = "Building not yet unlocked.";
+            }
+
+            //If we're working with a Building 3 cell:
+            if (cells[i].id == "building3") {
+
+                //If Building 3 is actually unlocked:
+                if (buildings.building3.unlocked) {
+
+                    //If we actually have the upgrade.. (This should be changed)
+                    if (upgradeIndex + 1 <= buildings.building3.upgrades.length) {
+                        cells[i].innerHTML = buildings.building3.upgrades[upgradeIndex].name;
+                    }
+
+                    //If we do not have the upgrade:
+                    else
+                        cells[i].innerHTML = "Upgrade not yet unlocked.";
+                }
+
+                else
+                    cells[i].innerHTML = "Building not yet unlocked.";
+            }
+
+            //If we're working with a Building 4 cell:
+            if (cells[i].id == "building4") {
+
+                //If Building 1 is actually unlocked:
+                if (buildings.building4.unlocked) {
+
+                    //If we actually have the upgrade.. (This should be changed)
+                    if (upgradeIndex + 1 <= buildings.building4.upgrades.length) {
+                        cells[i].innerHTML = buildings.building4.upgrades[upgradeIndex].name;
+                    }
+
+                    //If we do not have the upgrade:
+                    else
+                        cells[i].innerHTML = "Upgrade not yet unlocked.";
+                }
+
+                else
+                    cells[i].innerHTML = "Building not yet unlocked.";
+            }
+
+            //If we're working with a Building 5 cell:
+            if (cells[i].id == "building5") {
+
+                //If Building 5 is actually unlocked:
+                if (buildings.building5.unlocked) {
+
+                    //If we actually have the upgrade.. (This should be changed)
+                    if (upgradeIndex + 1 <= buildings.building5.upgrades.length) {
+                        cells[i].innerHTML = buildings.building5.upgrades[upgradeIndex].name;
+                    }
+
+                    //If we do not have the upgrade:
+                    else
+                        cells[i].innerHTML = "Upgrade not yet unlocked.";
+                }
+
+                else
+                    cells[i].innerHTML = "Building not yet unlocked.";
+            }
+
+            if (i >= 4 && i % 4 == 0)
+                upgradeIndex++;
+        }
+
+    }
+
+
+    refreshBuildings(); //initially load the buildings
+
+    //Debug output; just to make sure our first cell is getting set correctly
+    console.log(cells.get(0).id);
+
+
+    //Set all of the building names into the proper cells
+    header1.innerHTML = buildings.building1.name;
+    header2.innerHTML = buildings.building2.name;
+    header3.innerHTML = buildings.building3.name;
+    header4.innerHTML = buildings.building4.name;
+    header5.innerHTML = buildings.building5.name;
+
+}
 
 function updateAll(){
 	
@@ -44,8 +198,8 @@ function updateAll(){
 	waterPlantCapacityDiv.innerHTML = "Water Plant Initial Capacity: " + waterPlantCapacity + " gallons";
 	waterPlantNetCapacityDiv.innerHTML = "Water Plant Net Capacity: " + waterNetCapacity + " gallons";
 	
-	irrigateAtNightDiv.innerHTML = upgrades.irrigateAtNight.name + (25 * upgrades.irrigateAtNight.count) + "% of campus currently";
-	irrigateLessDiv.innerHTML = upgrades.irrigateLess.name + (25 * upgrades.irrigateLess.count) + "% of campus currently";
+//	irrigateAtNightDiv.innerHTML = upgrades.irrigateAtNight.name + (25 * upgrades.irrigateAtNight.count) + "% of campus currently";
+//	irrigateLessDiv.innerHTML = upgrades.irrigateLess.name + (25 * upgrades.irrigateLess.count) + "% of campus currently";
 	dualFlushToiletDiv.innerHTML = upgrades.dualFlushToilet.name + (25 * upgrades.dualFlushToilet.count) + "% of campus currently";
 	faucetSensorsDiv.innerHTML = upgrades.faucetSensors.name + (25 * upgrades.faucetSensors.count) + "% of campus currently";
 	greenRoofDiv.innerHTML = upgrades.greenRoof.name + (25 * upgrades.greenRoof.count) + "% of campus currently";
@@ -54,11 +208,11 @@ function updateAll(){
 	rainAmount.innerHTML = rainfall() + " inches of rain fell today ";
 	
 	//Reset everything back to "black" color, and selected as 0.
-	irrigateAtNightDiv.style.color = "black";
-	upgrades.irrigateAtNight.selected = 0;
-	
-	irrigateLessDiv.style.color = "black";
-	upgrades.irrigateLess.selected = 0;
+//	irrigateAtNightDiv.style.color = "black";
+//	upgrades.irrigateAtNight.selected = 0;
+//
+//	irrigateLessDiv.style.color = "black";
+//	upgrades.irrigateLess.selected = 0;
 	
 	dualFlushToiletDiv.style.color = "black";
 	upgrades.dualFlushToilet.selected = 0;
@@ -76,43 +230,43 @@ function updateAll(){
 
 
 
-//This function deals with and checks the conditions for adding 25% upgrades.irrigateAtNight.
-function addNightIrrigation(){
-	
-	if(canPlay() && upgrades.irrigateAtNight.count < 4){
-		
-		modifyBudget( (upgrades.irrigateAtNight.cost + upgrades.irrigateAtNight.costMaintain));
-		modifyWaterWasteRate(upgrades.irrigateAtNight.waterSaved);
-		modifySatisfaction(upgrades.irrigateAtNight.satisfaction);
-				
-		upgrades.irrigateAtNight.count++;
-		irrigateAtNightDiv.innerHTML = upgrades.irrigateAtNight.name + (25 * upgrades.irrigateAtNight.count) + 
-											"% of campus currently";
-
-		upgrades.irrigateAtNight.percentage += 5;	
-		irrigateNightBarDiv.style.width = "" + upgrades.irrigateAtNight.percentage + "%";
-	}
-
-}
-
-//This function deals with and checks the conditions for adding 25% lessIrrigation.
-function addLessIrrigation(){
-	
-	if(canPlay() && upgrades.irrigateLess.count < 4){
-		
-		modifyBudget( (upgrades.irrigateLess.cost + upgrades.irrigateLess.costMaintain));
-		modifyWaterWasteRate(upgrades.irrigateLess.waterSaved);
-		modifySatisfaction(upgrades.irrigateLess.satisfaction);
-				
-		upgrades.irrigateLess.count++;
-		irrigateLessDiv.innerHTML = upgrades.irrigateLess.name + (25 * upgrades.irrigateLess.count) + 
-											"% of campus currently";
-
-		upgrades.irrigateLess.percentage += 5;	
-		irrigateLessBarDiv.style.width = "" + upgrades.irrigateLess.percentage + "%";
-	}
-
-}
+////This function deals with and checks the conditions for adding 25% upgrades.irrigateAtNight.
+//function addNightIrrigation(){
+//
+//	if(canPlay() && upgrades.irrigateAtNight.count < 4){
+//
+//		modifyBudget( (upgrades.irrigateAtNight.cost + upgrades.irrigateAtNight.costMaintain));
+//		modifyWaterWasteRate(upgrades.irrigateAtNight.waterSaved);
+//		modifySatisfaction(upgrades.irrigateAtNight.satisfaction);
+//
+//		upgrades.irrigateAtNight.count++;
+//		irrigateAtNightDiv.innerHTML = upgrades.irrigateAtNight.name + (25 * upgrades.irrigateAtNight.count) +
+//											"% of campus currently";
+//
+//		upgrades.irrigateAtNight.percentage += 5;
+//		irrigateNightBarDiv.style.width = "" + upgrades.irrigateAtNight.percentage + "%";
+//	}
+//
+//}
+//
+////This function deals with and checks the conditions for adding 25% lessIrrigation.
+//function addLessIrrigation(){
+//
+//	if(canPlay() && upgrades.irrigateLess.count < 4){
+//
+//		modifyBudget( (upgrades.irrigateLess.cost + upgrades.irrigateLess.costMaintain));
+//		modifyWaterWasteRate(upgrades.irrigateLess.waterSaved);
+//		modifySatisfaction(upgrades.irrigateLess.satisfaction);
+//
+//		upgrades.irrigateLess.count++;
+//		irrigateLessDiv.innerHTML = upgrades.irrigateLess.name + (25 * upgrades.irrigateLess.count) +
+//											"% of campus currently";
+//
+//		upgrades.irrigateLess.percentage += 5;
+//		irrigateLessBarDiv.style.width = "" + upgrades.irrigateLess.percentage + "%";
+//	}
+//
+//}
 
 
 //This function deals with and checks the conditions for adding 25% upgrades.dualFlushToilet.
@@ -224,35 +378,35 @@ function modifySatisfaction(amount){
 
 }
 
-//Toggles whether or not upgrades.irrigateAtNight gets added in the next turn.
-function toggleIrrigateAtNight(){
-	if(upgrades.irrigateAtNight.selected == 1){
-		upgrades.irrigateAtNight.selected = 0;
-		irrigateAtNightDiv.style.color = "black";
-		console.log("Unselected upgrades.irrigateAtNight.");
-	}
-	
-	else{
-		upgrades.irrigateAtNight.selected = 1;
-		irrigateAtNightDiv.style.color = "green";
-		console.log("Selected upgrades.irrigateAtNight.");
-	}
-}
-
-//Toggles whether or not upgrades.irrigateLess gets added in the next turn.
-function toggleIrrigateLess(){
-	if(upgrades.irrigateLess.selected == 1){
-		upgrades.irrigateLess.selected = 0;
-		irrigateLessDiv.style.color = "black";
-		console.log("Unselected upgrades.irrigateLess.");
-	}
-	
-	else{
-		upgrades.irrigateLess.selected = 1;
-		irrigateLessDiv.style.color = "green";
-		console.log("Selected upgrades.irrigateLess.");
-	}
-}
+////Toggles whether or not upgrades.irrigateAtNight gets added in the next turn.
+//function toggleIrrigateAtNight(){
+//	if(upgrades.irrigateAtNight.selected == 1){
+//		upgrades.irrigateAtNight.selected = 0;
+//		irrigateAtNightDiv.style.color = "black";
+//		console.log("Unselected upgrades.irrigateAtNight.");
+//	}
+//
+//	else{
+//		upgrades.irrigateAtNight.selected = 1;
+//		irrigateAtNightDiv.style.color = "green";
+//		console.log("Selected upgrades.irrigateAtNight.");
+//	}
+//}
+//
+////Toggles whether or not upgrades.irrigateLess gets added in the next turn.
+//function toggleIrrigateLess(){
+//	if(upgrades.irrigateLess.selected == 1){
+//		upgrades.irrigateLess.selected = 0;
+//		irrigateLessDiv.style.color = "black";
+//		console.log("Unselected upgrades.irrigateLess.");
+//	}
+//
+//	else{
+//		upgrades.irrigateLess.selected = 1;
+//		irrigateLessDiv.style.color = "green";
+//		console.log("Selected upgrades.irrigateLess.");
+//	}
+//}
 
 //Toggles whether or not upgrades.dualFlushToilet gets added in the next turn.
 function toggleDualFlushToilet(){
@@ -324,20 +478,20 @@ function turnPass(){
 	//addNightIrrigation();
 	 
 	//Make any changes that the user has toggled based on the buttons pressed
-	
-	if(irrigateAtNightDiv.style.color == "green"){
-		if(addNightIrrigation() ) //attempt to add upgrades.irrigateAtNight
-			console.log("Added upgrades.irrigateAtNight 25%.");
-		else
-			irrigateAtNightDiv.style.color == "black";
-	}
-	
-	if(irrigateLessDiv.style.color == "green"){
-		if(addLessIrrigation() ) //attempt to add upgrades.irrigateLess
-			console.log("Added upgrades.irrigateLess 25%.");
-		else
-			irrigateLessDiv.style.color == "black";
-	}
+//
+//	if(irrigateAtNightDiv.style.color == "green"){
+//		if(addNightIrrigation() ) //attempt to add upgrades.irrigateAtNight
+//			console.log("Added upgrades.irrigateAtNight 25%.");
+//		else
+//			irrigateAtNightDiv.style.color == "black";
+//	}
+//
+//	if(irrigateLessDiv.style.color == "green"){
+//		if(addLessIrrigation() ) //attempt to add upgrades.irrigateLess
+//			console.log("Added upgrades.irrigateLess 25%.");
+//		else
+//			irrigateLessDiv.style.color == "black";
+//	}
 	
 	if(dualFlushToiletDiv.style.color == "green"){
 		if(addDualFlushToilet() ) //attempt to add dualFlushToilets
@@ -370,12 +524,12 @@ function turnPass(){
 	
 	
 	//Re-"Bill" for maintenance costs of each type of device
-	if(irrigateAtNightDiv.style.color == "black"){
-		modifyBudget(upgrades.irrigateAtNight.costMaintain * upgrades.irrigateAtNight.count);
-	}
-	if(irrigateLessDiv.style.color == "black"){
-		modifyBudget(upgrades.irrigateLess.costMaintain * upgrades.irrigateLess.count);
-	}
+//	if(irrigateAtNightDiv.style.color == "black"){
+//		modifyBudget(upgrades.irrigateAtNight.costMaintain * upgrades.irrigateAtNight.count);
+//	}
+//	if(irrigateLessDiv.style.color == "black"){
+//		modifyBudget(upgrades.irrigateLess.costMaintain * upgrades.irrigateLess.count);
+//	}
 	if(dualFlushToiletDiv.style.color == "black"){
 		modifyBudget(upgrades.dualFlushToilet.costMaintain * upgrades.dualFlushToilet.count);
 	}
@@ -415,12 +569,12 @@ function resetGame(){
 		calculateScore());
 
 	budget = initialBudget; 
-	timeSurvived = initialtimeSurvived;
+	timeSurvived = 0;
 	satisfaction = initialSatisfaction;
 	waterWasteRate = initialWaterWasteRate;
 	waterPlantCapacity = initialWaterPlantCapacity;
-	upgrades.irrigateAtNight.count = 0;
-	upgrades.irrigateLess.count = 0;
+//	upgrades.irrigateAtNight.count = 0;
+//	upgrades.irrigateLess.count = 0;
 	upgrades.dualFlushToilet.count = 0;
 	upgrades.faucetSensors.count = 0;
 	upgrades.greenRoof.count = 0;
@@ -432,8 +586,7 @@ function resetGame(){
 
 //This is completely arbitrary right now, just to see some numbers at the end. 
 function calculateScore(){
-	return (timeSurvived + (upgrades.irrigateAtNight.count * 2) + (satisfaction * 10) +
-			(upgrades.irrigateLess.count * 2) + (upgrades.dualFlushToilet.count * 2) + (upgrades.faucetSensors.count * 2) + 
+	return (timeSurvived + (satisfaction * 10) + (upgrades.dualFlushToilet.count * 2) + (upgrades.faucetSensors.count * 2) +
 			(upgrades.greenRoof.count * 2) + (upgrades.cistern.count * 2));
 }
 
